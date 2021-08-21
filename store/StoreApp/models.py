6c9819b04aa0846e0db46b78ef3cf33174805318
Stanislav <dartonetorr@gmail.com>
2021-08-21 18:08:15 +0300

    changing models

--- store/StoreApp/models.py
+++ store/StoreApp/models.py
@@ -3,15 +3,31 @@ from django.contrib.auth.models import AbstractUser
 
 
 # My models
-class Articles(models.Model):
+class Product(models.Model):
+    SMARTPHONES = 'Smartphones'
+    LAPTOPS = 'Laptops'
+    GADGETS = 'Gadgets'
+    AUDIO = 'Audio'
+    PHOTO_VIDEO = 'Photo_Video'
+    CATEGORY_CHOICES = [
+        (SMARTPHONES, 'Smartphones'),
+        (LAPTOPS, 'Laptops'),
+        (GADGETS, 'Gadgets'),
+        (AUDIO, 'Audio'),
+        (PHOTO_VIDEO, 'Photo_Video'),
+    ]
+    # slug = models.SlugField(max_length=40, unique=True)
+    product_category = models.CharField(choices=CATEGORY_CHOICES, max_length=25)
+    image = models.ImageField(verbose_name='Product image')
     create_date = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=120, verbose_name='Product name')
-    text = models.TextField(max_length=250, verbose_name='Product description')
+    text = models.TextField(max_length=999, verbose_name='Product description')
     price = models.PositiveIntegerField(verbose_name='Product price')
     amount = models.PositiveIntegerField(null=True, verbose_name='Amount of product')
 
     def __str__(self):
-        return self.name
+        return f'{self.product_category} ---  {self.name} ' \
+               f'--- Price: {self.price} --- Amount: {self.amount}'
 
     class Meta:
         verbose_name = 'Product'
@@ -30,7 +46,7 @@ class User(AbstractUser):
 
 class Purchase(models.Model):
     buyer = models.ForeignKey(User, related_name='buyer', on_delete=models.CASCADE)
-    product = models.ForeignKey(Articles, related_name='product', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE)
     buy_time = models.DateTimeField(auto_now=True)
     input_amount = models.PositiveIntegerField(default=1, verbose_name='Amount of product for user')
     final_price = models.PositiveIntegerField(verbose_name='Final price')
@@ -39,8 +55,8 @@ class Purchase(models.Model):
         return self.input_amount
 
     def __str__(self):
-        return f'Product: {self.product.name} --- Buyer: {self.buyer} --- Buy time: {self.buy_time}'
+        return f'{self.product.name} --- Buyer: {self.buyer} --- Amount: {self.input_amount}  ' \
+               f'--- Final price: {self.final_price} --- Buy time: {self.buy_time.strftime("%Y-%m-%d %H:%M:%S")}'
 
     def save(self, *args, **kwargs):
-        # self.final_price = self.input_amount * self.product.price
         super().save(*args, **kwargs)
