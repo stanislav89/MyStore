c61888d6073b476a8bd1612548600caafaee6a0b
Stanislav <dartonetorr@gmail.com>
2021-08-21 18:14:38 +0300

    upgrade for new models

--- store/StoreApp/urls.py
+++ store/StoreApp/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomePageListView, ArticleCreateView, AddProductView, ProductDetailView, ProductUpdateView, \
-    ProductDeleteView, StoreLoginView, RegisterUserView, UserLogoutView, PurchaseView, UserAccountView
+from .views import HomePageListView, ProductCreateView, AddProductView, ProductDetailView, \
+    ProductUpdateView, ProductDeleteView, StoreLoginView, RegisterUserView, UserLogoutView, PurchaseView, \
+    UserAccountView, SmartphonesListView, LaptopsListView, GadgetsListView, AudioListView, PhotoVideoListView
 
 urlpatterns = [
     path('', HomePageListView.as_view(), name='home'),
-    path('edit-page', ArticleCreateView.as_view(), name='edit_page'),
+    path('smartphones/', SmartphonesListView.as_view(), name='smartphones'),
+    path('laptops/', LaptopsListView.as_view(), name='laptops'),
+    path('gadgets/', GadgetsListView.as_view(), name='gadgets'),
+    path('audio/', AudioListView.as_view(), name='audio'),
+    path('photo-video/', PhotoVideoListView.as_view(), name='photo_video'),
+    path('edit-page', ProductCreateView.as_view(), name='edit_page'),
     path('create', AddProductView.as_view(), name='create'),
     path('<int:pk>/', ProductDetailView.as_view(), name='description'),
     path('<int:pk>/update', ProductUpdateView.as_view(), name='update'),
